5d79bb4e80388712dcb686b1f48f03055ed3be67
Pirasp <johanneshofer@gmx.net>
2023-01-13 22:25:45 +0100

    migration zu thymeleaf
    weil views aus irgend einem grund versuchen bilder zu strings zu konvertieren...
    add lexicon entry immer noch broken

--- src/main/java/de/mushroomfinder/controller/MapController.java
+++ src/main/java/de/mushroomfinder/controller/MapController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MapController {
+	@RequestMapping("/")
+	public String start(){
+		return"map";
+	}
 	
 	@RequestMapping("/map")
 	public String showContact() {
--- src/main/java/de/mushroomfinder/controller/MushroomController.java
+++ src/main/java/de/mushroomfinder/controller/MushroomController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -20,19 +22,23 @@ public class MushroomController {
     private final MushroomLexiconAddService mushroomLexiconAddService;
     private final MushroomLexiconService mushroomLexiconService;
 
-    public MushroomController(MushroomLexiconAddService mushroomLexiconAddService, MushroomLexiconService mushroomLexiconService) {
+    public MushroomController(MushroomLexiconAddService mushroomLexiconAddService,
+                              MushroomLexiconService mushroomLexiconService) {
+
         this.mushroomLexiconAddService = mushroomLexiconAddService;
         this.mushroomLexiconService = mushroomLexiconService;
     }
 
     @GetMapping("/lexicon")
     public ModelAndView lexicon(@RequestParam(value = "searchTerm", defaultValue = " ") String searchTerm){
+
         List<Mushroom> mushrooms = mushroomLexiconService.searchMushrooms(searchTerm);
         return new ModelAndView("lexicon", "mushrooms", mushrooms);
     }
 
     @GetMapping(value = "/mushrooms/picture/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
     public @ResponseBody String getMushroomPicture(@PathVariable("id") Long id) {
+
         Mushroom mushroom = mushroomLexiconService.getMushroomById(id);
         return mushroom.getPicture();
     }
@@ -44,16 +50,27 @@ public class MushroomController {
     }
 
     @PostMapping("/lexicon/add")
-    public String addMushroom(@RequestParam("picture") MultipartFile picture, @ModelAttribute Mushroom mushroom) throws IOException {
-        if (!picture.isEmpty()) {
-            try {
-                picture.transferTo(new File("src/main/resources/static/pictures/" + picture.getOriginalFilename()));
-                mushroom.setPicture(picture.getOriginalFilename());
-                mushroomLexiconAddService.addMushroom(mushroom);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    public String addMushroom(@RequestParam("picture") MultipartFile picture,
+                              @ModelAttribute Mushroom mushroom,
+                              RedirectAttributes redirectAttributes,
+                              HttpServletRequest request) throws IOException {
+
+        String fileName = picture.getOriginalFilename();
+        String path = request.getServletContext().getRealPath("/");
+        File uploadDir = new File(path + "/images/");
+        if (!uploadDir.exists()) {
+            uploadDir.mkdirs();
         }
+        // Create a file object to save the uploaded file
+        File uploadFile = new File(uploadDir.getPath() + File.separator + fileName);
+        try {
+            // Save the uploaded file
+            picture.transferTo(uploadFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Set the file name as a redirect attribute
+        redirectAttributes.addAttribute("fileName", fileName);
 
         mushroomLexiconAddService.addMushroom(mushroom);
         return "redirect:/lexicon";
