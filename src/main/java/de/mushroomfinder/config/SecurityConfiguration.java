5227ad5c0ed27e5317232123fe25199648302b3d
xaibohphobiax <ogtimes@web.de>
2023-01-20 12:24:32 +0100

    security update

--- src/main/java/de/mushroomfinder/config/SecurityConfiguration.java
+++ src/main/java/de/mushroomfinder/config/SecurityConfiguration.java
@@ -76,16 +76,20 @@ public class SecurityConfiguration{
     }
     /*@Override*/
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
+        http.csrf()
+                .disable()
+                .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/users").authenticated()
+                /*.antMatchers("/users").authenticated()*/
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                    .usernameParameter("email")
-                    .defaultSuccessUrl("/users")
-                    .permitAll()
+                    /*.usernameParameter("email")*/
+                .loginPage("/login")
+                .loginProcessingUrl("/perform_login")
+                .defaultSuccessUrl("/map", true)
+                .failureUrl("/login.html?error=true")
+                    /*.permitAll()*/
                 .and()
                 .logout().logoutSuccessUrl("/").permitAll();
     }
