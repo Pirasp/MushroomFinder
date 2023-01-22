e84b63324ad9fcb8399d2a46ed05f6791d1752cb
korbinian.meier <50988510+mek46163@users.noreply.github.com>
2023-01-22 15:36:59 +0100

    deleted not used imports

--- src/main/java/de/mushroomfinder/config/SecurityConfiguration.java
+++ src/main/java/de/mushroomfinder/config/SecurityConfiguration.java
@@ -1,21 +1,13 @@
 package de.mushroomfinder.config;
 
-import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import de.mushroomfinder.service.MyUserDetailService;
@@ -24,8 +16,7 @@ import de.mushroomfinder.service.MyUserDetailService;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-	   @Autowired
-	    private DataSource dataSource;
+
 	     
 	    @Bean
 	    public UserDetailsService userDetailsService() {
