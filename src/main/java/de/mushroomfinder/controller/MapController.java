d5c79d01c590b529c534b3802a08ff1bddc1b4de
Pirasp <johanneshofer@gmx.net>
2023-01-26 15:03:04 +0100

    addendum to last commit

--- src/main/java/de/mushroomfinder/controller/MapController.java
+++ src/main/java/de/mushroomfinder/controller/MapController.java
@@ -4,13 +4,19 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.mushroomfinder.entities.Marker;
 import de.mushroomfinder.repository.MarkerRepository;
+import de.mushroomfinder.repository.MushroomSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import de.mushroomfinder.entities.Spot;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.List;
 
 
@@ -33,14 +39,14 @@ public class MapController {
 
 
 	//ADDMAP
-	@GetMapping("/addmap")
+	@GetMapping("/map/add")
 	public String showContact(Model model/*, @RequestParam("id") Long id*/) {
 		/*model.addAttribute("mushroomspot", mushroomSpotRepository.findById(id));*/
 		model.addAttribute("spot", new Spot());
 		return "addmap";
 	}
 	/*lang long from marker and id from user*/
-	@PostMapping("/addmap")
+	@PostMapping("/map/add")
 	public String addMarker(@ModelAttribute Spot spot,
 							@RequestParam("name") String name,
 							@RequestParam("picture") MultipartFile picture,
@@ -56,19 +62,19 @@ public class MapController {
 			}
 			spot.setPicture(tmp);
 		} catch (IOException e) {
-			return "redirect:/addmap/errorPictureSize";
+			return "redirect:/map/add/errorPictureSize";
 		}
 		spot.setDescription(description);
 		spot.setLatitude(latitude);
 		spot.setLongitude(longitude);
 		mushroomSpotRepository.save(spot);
-		return "redirect:/addmap";
+		return "redirect:/map/add";
 
 	}
 
-	@RequestMapping("/addmap/errorPictureSize")
+	@RequestMapping("/map/add/errorPictureSize")
 	public String errorPicsize(){
-		return "/errorPictureSize";
+		return "/errorPictureSize_addMap";
 	}
 
 	@InitBinder
